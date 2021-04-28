812ade60d489031d68922ecbe5384c42675ce92a
jj-krehbiel <72628524+jj-krehbiel@users.noreply.github.com>
2021-04-27 20:43:52 -0400

    clean up comments

--- doughnut chart/static/js/logic2.js
+++ doughnut chart/static/js/logic2.js
@@ -1,8 +1,14 @@
 var url = "http://127.0.0.1:5000/api/v1.0/launch_data"
+// var payload = []
 var Satellite = 0;
 var Dragon = 0;
+// launch_data = d3.json(url).then(function(launch_data){
+//     for (var i = 0; i<launch_data.length; i++) {
+//         payload.push(launch_data[i].Payload_type);
+//     }
+// });
+// console.log(payload);
 
-var url = "http://127.0.0.1:5000/api/v1.0/launch_data"
 d3.json(url).then(function (launch_data) {
     for (var i = 0; i < launch_data.length; i++) {
         if (launch_data[i].Payload_type === 'Satellite') {
@@ -14,19 +20,58 @@ d3.json(url).then(function (launch_data) {
     }
     console.log("Satellite: " + Satellite);
     console.log("Dragon: " + Dragon);
+
+});
+
+    // console.log("Satellite: " + Satellite);
+    // console.log("Dragon: " + Dragon);
+
+    // var Satellite = 0;
+    // var Dragon = 0;
+    // for (var i = 0; i<payload.length; i++) {
+    //     if (payload[i]=== "Satellite") {
+    //         Satellite+= 1
+    //     }
+    //     else {
+    //         Dragon+=1
+    //     };
+
+    // };
+    // console.log("Satellite: " + Satellite)
+    // console.log("Dragon: " + Dragon);
+
+
+    // var ctx = document.getElementById("myChart").getContext('2d')
+
+    // var chart = new Chart(ctx, {
+    //     type:'bar',
+    //     data: {
+    //         // labels: ['boston', 'worcester', 'springfield', 'lowell', 'cambridge', 'new bedford'],
+    //         labels: payload,
+    //         datasets: [{
+    //             label: 'Population',
+    //             data: payload
+    //         }]
+    //     },
+    // });
+
+
     // Bar chart from chart.js tutorial
 
-    var ctx = document.getElementById("myChart").getContext('2d')
+    var ctx = document.getElementById("myChart2").getContext('2d')
 
     // Chart.defaults.global.defaultFontSize = 18;
 
-    var chartx = new Chart(ctx, {
+    new Chart(ctx, {
         type: 'doughnut',
         data: {
             labels: ['Satellite', 'Dragon'],
             datasets: [{
                 label: 'Payload Type',
-                data: [Satellite, Dragon],
+                data: [
+                    83,
+                    28
+                ],
                 backgroundColor: ['blue', 'orange'],
                 tooltip_line1: ['SpaceX has launched a number of satellites for both', 'Dragon spacecrafts are capable of carrying humans'],
                 tooltip_line2: ['governmental space agencies and private corporations.', 'and cargo to and from orbit and beyond.'],
@@ -70,48 +115,6 @@ d3.json(url).then(function (launch_data) {
                 }
             }
         }
-        
+
     });
-
-    updateConfigAsNewObject(chartx);
-    updateConfigByMutating(chartx);
-
-});
-
-function updateConfigByMutating(chart) {
-    chart.options.plugins.title.text = 'new title';
-    chart.update();
-}
-
-function updateConfigAsNewObject(chart) {
-    chart.options = {
-        title: {
-            display: true,
-            text: 'Mission Type',
-            fontSize: 25
-        },
-        tooltips: {
-            displayColors: false,
-            callbacks: {
-                title: function (tooltipItem, data) {
-                    return data['labels'][tooltipItem[0]['index']];
-                },
-
-                beforeLabel: function (tooltipItem, data) {
-                    var dataset = data['datasets'][0];
-                    var percent = Math.round((dataset['data'][tooltipItem['index']] / dataset["_meta"][0]['total']) * 100)
-                    return data['datasets'][0]['data'][tooltipItem['index']] + ' (' + percent + '%)';
-                },
-
-                label: function (tooltipItem, data) {
-                    return data['datasets'][0]['tooltip_line1'][tooltipItem['index']];
-                },
-                afterLabel: function (tooltipItem, data) {
-                    return data['datasets'][0]['tooltip_line2'][tooltipItem['index']];
-                }
-            }
-        }
-    }
-    chart.update();
-}
 
