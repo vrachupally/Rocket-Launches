b9ae0699543756a918409ef3983f9023475b53ca
Sherali <sheraliyadav1993@gmail.com>
2021-04-29 23:32:17 -0400

    working range data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,44 +1,46 @@
 var url = "http://127.0.0.1:5000/api/v1.0/launch_data"
 var success = {
     2006: 0,
-    2007:0,
-    2008:0,
-    2009:0,
-    2010:0,
-    2011:0,
-    2012:0,
-    2013:0,
-    2014:0,
-    2015:0,
-    2016:0,
-    2017:0,
-    2018:0,
-    2019:0,
-    2020:0
+    2007: 0,
+    2008: 0,
+    2009: 0,
+    2010: 0,
+    2011: 0,
+    2012: 0,
+    2013: 0,
+    2014: 0,
+    2015: 0,
+    2016: 0,
+    2017: 0,
+    2018: 0,
+    2019: 0,
+    2020: 0
 };
 
 var failure = {
     2006: 0,
-    2007:0,
-    2008:0,
-    2009:0,
-    2010:0,
-    2011:0,
-    2012:0,
-    2013:0,
-    2014:0,
-    2015:0,
-    2016:0,
-    2017:0,
-    2018:0,
-    2019:0,
-    2020:0
+    2007: 0,
+    2008: 0,
+    2009: 0,
+    2010: 0,
+    2011: 0,
+    2012: 0,
+    2013: 0,
+    2014: 0,
+    2015: 0,
+    2016: 0,
+    2017: 0,
+    2018: 0,
+    2019: 0,
+    2020: 0
 };
 
 
 var url = "http://127.0.0.1:5000/api/v1.0/launch_data"
+
 var Satellite = 0;
 var Dragon = 0;
+
 // var bar_data = {
 //     year: [],
 //     number_of_launches: []
@@ -46,6 +48,7 @@ var Dragon = 0;
 var year = []
 
 var url = "http://127.0.0.1:5000/api/v1.0/launch_data"
+
 d3.json(url).then(function (launch_data) {
     console.log("THE DATA: ", launch_data)
     var nationality = new Set(launch_data.map(e => e.Nationality));
@@ -53,109 +56,40 @@ d3.json(url).then(function (launch_data) {
     function getItems(input) {
         var arr = input, obj = {};
         for (var i = 0; i < arr.length; i++) {
-          if (!obj[arr[i].Nationality]) {
-            obj[arr[i].Nationality] = 1;
-          } else if (obj[arr[i].Nationality]) {
-            obj[arr[i].Nationality] += 1;
-          }
+            if (!obj[arr[i].Nationality]) {
+                obj[arr[i].Nationality] = 1;
+            } else if (obj[arr[i].Nationality]) {
+                obj[arr[i].Nationality] += 1;
+            }
         }
         return obj;
     };
     var nationalities = getItems(launch_data);
     console.log(nationalities);
 
-    for (var i = 0; i < launch_data.length; i++) {
-        if (launch_data[i].launch_year === '2006' && launch_data[i].launch_success ===false) {
-            failure[2006] += 1
-        }
-        else if (launch_data[i].launch_year === '2007' && launch_data[i].launch_success ===false) {
-            failure[2007] += 1
-        }
-        else if (launch_data[i].launch_year === '2008' && launch_data[i].launch_success ===false) {
-            failure[2008] += 1
-        }
-        else if (launch_data[i].launch_year === '2009' && launch_data[i].launch_success ===false) {
-            failure[2009] += 1
-        }
-        else if (launch_data[i].launch_year === '2010' && launch_data[i].launch_success ===false) {
-            failure[2010] += 1
-        }
-        else if (launch_data[i].launch_year === '2011' && launch_data[i].launch_success ===false) {
-            failure[2011] += 1
-        }
-        else if (launch_data[i].launch_year === '2012' && launch_data[i].launch_success ===false) {
-            failure[2012] += 1
-        }
-        else if (launch_data[i].launch_year === '2013' && launch_data[i].launch_success ===false) {
-            failure[2012] += 1
-        }
-        else if (launch_data[i].launch_year === '2014' && launch_data[i].launch_success ===false) {
-            failure[2014] += 1
-        }
-        else if (launch_data[i].launch_year === '2015' && launch_data[i].launch_success ===false) {
-            failure[2015] += 1
-        }
-        else if (launch_data[i].launch_year === '2016' && launch_data[i].launch_success ===false) {
-            failure[2016] += 1
-        }
-        else if (launch_data[i].launch_year === '2017' && launch_data[i].launch_success ===false) {
-            failure[2017] += 1
-        }
-        else if (launch_data[i].launch_year === '2018' && launch_data[i].launch_success ===false) {
-            failure[2018] += 1
-        }
-        else if (launch_data[i].launch_year === '2019' && launch_data[i].launch_success ===false) {
-            failure[2019] += 1
-        }
-        else if (launch_data[i].launch_year === '2020' && launch_data[i].launch_success ===false) {
-            failure[2020] += 1
-        }
-        else if (launch_data[i].launch_year === '2006' && launch_data[i].launch_success ===true) {
-            success[2006] += 1
-        }
-        else if (launch_data[i].launch_year === '2007' && launch_data[i].launch_success ===true) {
-            success[2007] += 1
-        }
-        else if (launch_data[i].launch_year === '2008' && launch_data[i].launch_success ===true) {
-            success[2008] += 1
-        }
-        else if (launch_data[i].launch_year === '2009' && launch_data[i].launch_success ===true) {
-            success[2009] += 1
-        }
-        else if (launch_data[i].launch_year === '2010' && launch_data[i].launch_success ===true) {
-            success[2010] += 1
-        }
-        else if (launch_data[i].launch_year === '2011' && launch_data[i].launch_success ===true) {
-            success[2011] += 1
-        }
-        else if (launch_data[i].launch_year === '2012' && launch_data[i].launch_success ===true) {
-            success[2012] += 1
-        }
-        else if (launch_data[i].launch_year === '2013' && launch_data[i].launch_success ===true) {
-            success[2012] += 1
-        }
-        else if (launch_data[i].launch_year === '2014' && launch_data[i].launch_success ===true) {
-            success[2014] += 1
-        }
-        else if (launch_data[i].launch_year === '2015' && launch_data[i].launch_success ===true) {
-            success[2015] += 1
-        }
-        else if (launch_data[i].launch_year === '2016' && launch_data[i].launch_success ===true) {
-            success[2016] += 1
-        }
-        else if (launch_data[i].launch_year === '2017' && launch_data[i].launch_success ===true) {
-            success[2017] += 1
-        }
-        else if (launch_data[i].launch_year === '2018' && launch_data[i].launch_success ===true) {
-            success[2018] += 1
-        }
-        else if (launch_data[i].launch_year === '2019' && launch_data[i].launch_success ===true) {
-            success[2019] += 1
-        }
-        else if (launch_data[i].launch_year === '2020' && launch_data[i].launch_success ===true) {
-            success[2020] += 1
-        }
-    }
+    console.log("Launch Data: ", launch_data)
+
+    // for each row 
+
+    const scrubbedData = launch_data.filter((launch) => { return launch.launch_success === true || launch.launch_success === false })
+
+    const successfulLaunches = scrubbedData.filter((launch) => { return launch.launch_success === true })
+    const failedLaunches = scrubbedData.filter((launch) => { return launch.launch_success === false })
+
+    successfulLaunches.forEach((launch) => {
+        success[`${launch.launch_year}`] += 1;
+    })
+
+    failedLaunches.forEach((launch) => {
+        failure[`${launch.launch_year}`] += 1;
+    })
+
+    console.log("SUCCESS OBJ: ", success)
+
+    console.log("FAILURE OBJ: ", failure)
+
+
+
 
     for (var i = 0; i < launch_data.length; i++) {
         if (launch_data[i].Payload_type === 'Satellite') {
@@ -167,6 +101,7 @@ d3.json(url).then(function (launch_data) {
         // loop through data. count number of lauches for each year. take the year and if it's unique, push it to the year array. then we want to add 1 to the number of launch
         year.push(launch_data[i].launch_year)
     }
+
     console.log(launch_data)
     console.log(year)
     // Bar chart from chart.js tutorial
@@ -174,7 +109,7 @@ d3.json(url).then(function (launch_data) {
     var donut = document.getElementById("myChart").getContext('2d')
 
     // Chart.defaults.global.defaultFontSize = 18;
-    
+
     var donutChart = new Chart(donut, {
         type: 'doughnut',
         data: {
@@ -225,30 +160,39 @@ d3.json(url).then(function (launch_data) {
                 }
             }
         }
-        
+
     });
 
-    updateConfigAsNewObject(donutChart);
-    updateConfigByMutating(donutChart);
+    // updateConfigAsNewObject(donutChart);
+    // updateConfigByMutating(donutChart);
 
-    var ctx = document.getElementById("lineChart").getContext('2d')
+    var lineChart = document.getElementById("lineChart").getContext('2d')
 
+    // Needs to pass filted years after the launch button is presssed
 
-    new Chart(ctx, {
+    // User selects begin year
+    // store var
+    // User selects end year
+    // store var
+    // User presses launch button 
+    // take both stored vars and filter the data for everythign between those years "inclsive"
+    // Re render graph
+
+    new Chart(lineChart, {
         type: 'line',
         data: {
             labels: Object.keys(success),
             datasets: [{
                 label: 'Successful Launches',
-                data: Object.values(success), 
+                data: Object.values(success),
                 backgroundColor: '#1593ba',
                 borderWidth: 1,
                 borderColor: '#777',
                 hoverBorderWidth: 3,
                 hoverBorderColor: '#000000'
-            },{
+            }, {
                 label: 'Failed Launches',
-                data: Object.values(failure), 
+                data: Object.values(failure),
                 backgroundColor: '#d8c293',
                 borderWidth: 1,
                 borderColor: '#777',
@@ -264,10 +208,10 @@ d3.json(url).then(function (launch_data) {
             },
             tooltips: {
                 displayColors: false,
-               
+
             }
         }
-        
+
     });
 
     var bar = document.getElementById("barChart").getContext('2d')
@@ -278,7 +222,7 @@ d3.json(url).then(function (launch_data) {
             labels: Object.keys(nationalities),
             datasets: [{
                 label: 'SpaceX Customers',
-                data: Object.values(nationalities), 
+                data: Object.values(nationalities),
                 backgroundColor: '#07243c',
                 borderWidth: 1,
                 borderColor: '#777',
@@ -294,68 +238,110 @@ d3.json(url).then(function (launch_data) {
             },
             tooltips: {
                 displayColors: false,
-               
+
             }
         }
-        
+
     });
 
 });
 
-function updateConfigByMutating(chart) {
-    chart.options.plugins.title.text = 'new title';
-    chart.update();
-}
 
-function updateConfigAsNewObject(chart) {
-    chart.options = {
-        title: {
-            display: true,
-            text: 'Mission Type',
-            fontSize: 25
+function handleYearRange() {
+    console.log("I have been clicked")
+    console.log(beginYear, endYear)
+
+    // let successYears = {
+    //     2007: 0,
+    //     2008: 0,
+    //     2009: 0,
+    //     2010: 0,
+    //     2011: 0,
+    //     2012: 0,
+    //     2013: 0,
+    //     2014: 0,
+    //     2015: 0,
+    // };
+
+    let successYears = {}
+
+    // ["2007" - "2015"]
+    Object.keys(success).map((year) => {
+        console.log(year)
+
+        // beginYear = 2007
+        // endYear = 2015
+
+        // current iteration year = 2007
+        console.log("beginYear > year", beginYear > year)
+        console.log("endYear <  year", endYear < year)
+        console.log("beginYear > year && endYear <  year", beginYear > year && endYear < year)
+
+        
+        const inRange = beginYear > year && endYear < year;
+
+        // 
+
+        if(year >= beginYear && year <= endYear) {
+            successYears[`${year}`] = success[`${year}`]
+        } else {
+            return; 
+        };
+         
+
+        // if (inRange) {
+        //     console.log("IS IN RANGE: ", year)
+        //     return;
+        // } else {
+        //     // { 2007: whatever is already in success["2007"] }
+
+        //     successYears[`${year}`] = success[`${year}`]
+
+        // }
+
+
+    })
+
+    console.log("SUCCESS YEAR: ", successYears)
+
+    const lineChart = document.getElementById("lineChart").getContext('2d')
+
+
+    new Chart(lineChart, {
+        type: 'line',
+        data: {
+            labels: Object.keys(success),
+            datasets: [{
+                label: 'Successful Launches',
+                data: Object.values(success),
+                backgroundColor: '#1593ba',
+                borderWidth: 1,
+                borderColor: '#777',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000000'
+            }, {
+                label: 'Failed Launches',
+                data: Object.values(failure),
+                backgroundColor: '#d8c293',
+                borderWidth: 1,
+                borderColor: '#777',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000000'
+            }]
         },
-        tooltips: {
-            displayColors: false,
-            callbacks: {
-                title: function (tooltipItem, data) {
-                    return data['labels'][tooltipItem[0]['index']];
-                },
+        options: {
+            title: {
+                display: true,
+                text: 'Mission Type',
+                fontSize: 25
+            },
+            tooltips: {
+                displayColors: false,
 
-                beforeLabel: function (tooltipItem, data) {
-                    var dataset = data['datasets'][0];
-                    var percent = Math.round((dataset['data'][tooltipItem['index']] / dataset["_meta"][0]['total']) * 100)
-                    return data['datasets'][0]['data'][tooltipItem['index']] + ' (' + percent + '%)';
-                },
-
-                label: function (tooltipItem, data) {
-                    return data['datasets'][0]['tooltip_line1'][tooltipItem['index']];
-                },
-                afterLabel: function (tooltipItem, data) {
-                    return data['datasets'][0]['tooltip_line2'][tooltipItem['index']];
-                }
             }
         }
-    }
-    chart.update();
+
+    });
+
+    console.log(lineChart)
 }
-
-// var ctx = document.getElementById("myChart2").getContext('2d')
-// var chartx = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: year,
-//         datasets: [{
-            
-//             data: year,
-//             backgroundColor: ['blue'],
-//             tooltip_line1: ['SpaceX has launched a number of satellites for both', 'Dragon spacecrafts are capable of carrying humans'],
-//             tooltip_line2: ['governmental space agencies and private corporations.', 'and cargo to and from orbit and beyond.'],
-//             borderWidth: 1,
-//             borderColor: '#777',
-//             hoverBorderWidth: 3,
-//             hoverBorderColor: '#000000'
-//         }]
-//     }
-    
-// });
-
--- static/js/slider.js
+++ static/js/slider.js
@@ -1,54 +1,41 @@
 $(document).ready(function () {
     console.log("READY!");
-    console.log("READY!");
-    console.log("READY!");
-    console.log("READY!");
-    console.log("READY!");
-    console.log("READY!");
-
-
-    // const slider = $("#customRange1")[0].change(function(){
-    //     alert("The text has been changed.");
-    //   });
-
-
-
-    // console.log("slider: ", slider);
-
-    // const getRange = function () {
-    //     console.log("HERE")
-    // }
-
-    // slider.change(getRange())
 
     // initialize the data
     function buildDropdownOptions() {
         console.log("BUILD DROP DOWN RUNNING")
         // select the dropdown from DOM
-        const dropdownMenu = d3.select("#selDataset");
+        const beginYearDropDown = d3.select("#beginYear");
+        const endYearDropDown = d3.select("#endYear");
 
-        const years = ["2006", "2007", "2008", "2009", "2010"]
+        const years = ["2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]
 
         years.forEach((year) => {
             console.log("YEAR", year)
-            dropdownMenu.append('option').text(year).property('value', year);
+
+            beginYearDropDown.append('option').text(year).property('value', year);
+
+            endYearDropDown.append('option').text(year).property('value', year).property("selected", year);
         });
 
-        // promise pending
-        // const selectID = subjectID[0];
-
-        const initialYear = years[0];
-
-        // buildPlot(initialYear);
-        // updateMetadata(initialYear);
-        // console.log(subjectID);
-
     }
-
-    function optionChanged(selectNew) {
-        updateMetadata(selectNew);
-        buildPlot(selectNew);
-    }
-
     buildDropdownOptions()
 });
+
+let beginYear = 2006;
+let endYear = 2020;
+
+
+function handleBeginYear(value) {
+    console.log("Begin Year: ", value)
+    beginYear = value;
+
+}
+
+function handleEndYear(value) {
+    console.log("End Year: ", value)
+    endYear = value;
+}
+
+
+
